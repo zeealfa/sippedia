48ba3f1fcf9bbb807233053b97a0dd489e8eaa03
Zee <zee.alfa@gmail.com>
2017-03-19 04:43:12 +0700

    Cover up

--- index.php
+++ index.php
@@ -90,8 +90,24 @@ switch ($f[0]) {
                 array(
                     "kodeMedia" => 1,
                     "judulMedia" => "Jenis-jenis kelembagaan sosial di indonesia- IPS kelas 7",
-                    "pembuatMedia" => "Dita Ratna Pratiwi"
-                )
+                    "pembuatMedia" => "Dita Ratna Pratiwi",
+                    "tahunMedia"=>"",
+                    "fileMedia"=>"1.mp4"
+                ),
+                array(
+                    "kodeMedia" => 2,
+                    "judulMedia" => "Kecerdasan sosial - SMP kelas 8",
+                    "pembuatMedia" => "Imron Saefudin",
+                    "tahunMedia"=>"2016",
+                    "fileMedia"=>"2.png"
+                ),
+                array(
+                    "kodeMedia" => 3,
+                    "judulMedia" => "PENGARUH KEUNGGULAN LOKASI TERHADAP KOLONIALISME BARAT",
+                    "pembuatMedia" => "",
+                    "tahunMedia"=>"",
+                    "fileMedia"=>"3.exe"
+                ),
             )
         );
         $fileTarget = "utama";
@@ -102,10 +118,47 @@ switch ($f[0]) {
             "judul" => "Sekolah Menengah Atas",
             "media" => array(
                 array(
-                    "kodeMedia" => 2,
+                    "kodeMedia" => 4,
                     "judulMedia" => "Perawatan PC - SMK Kelas X",
-                    "pembuatMedia" => "Kanzul Fikri Fauzi"
-                )
+                    "pembuatMedia" => "Kanzul Fikri Fauzi",
+                    "tahunMedia"=>"",
+                    "fileMedia"=>"4.pptm"
+                ),
+                array(
+                    "kodeMedia" => 5,
+                    "judulMedia" => "Rekayasa perangkat lunak - SMK kelas 12",
+                    "pembuatMedia" => "Ary Santosa",
+                    "tahunMedia"=>"2016",
+                    "fileMedia"=>"5.png"
+                ),
+                array(
+                    "kodeMedia" => 6,
+                    "judulMedia" => "Teknik Telekomunikasi - SMK kelas 11",
+                    "pembuatMedia" => "Imam Mudi",
+                    "tahunMedia"=>"2016",
+                    "fileMedia"=>"6.jpg"
+                ),
+                array(
+                    "kodeMedia" => 7,
+                    "judulMedia" => "Mengasah kemampuan ekonomi - SMA kelas 10",
+                    "pembuatMedia" => "Jusuf Apandy",
+                    "tahunMedia"=>"2016",
+                    "fileMedia"=>"7.png"
+                ),
+                array(
+                    "kodeMedia" => 8,
+                    "judulMedia" => "menyelami fenomena sosial di masyarakat - SMA kelas 11",
+                    "pembuatMedia" => "Sony Faizal",
+                    "tahunMedia"=>"2016",
+                    "fileMedia"=>"8.jpg"
+                ),
+                array(
+                    "kodeMedia" => 9,
+                    "judulMedia" => "Bahasa Inggris - SMA kelas 12",
+                    "pembuatMedia" => "SIti Maisaroh",
+                    "tahunMedia"=>"2016",
+                    "fileMedia"=>"9.png"
+                ),
             )
 
         );
@@ -133,7 +186,7 @@ $globalVar = array(
 $menuAdmin = array(
     array('label' => 'SMP Sederajat', 'target' => 'smp', 'icon' => 'fa-file-text-o', 'warna' => '#1F92CA', 'isActive' => @$isActive['smp']),
     array('label' => 'SMA Sederajat', 'target' => 'sma', 'icon' => 'fa-file-text-o', 'warna' => '#536DFD', 'isActive' => @$isActive['sma']),
-    array('label' => 'Ditandai', 'target' => 'tag', 'icon' => 'fa-tag', 'warna' => '#FC6800', 'isActive' => @$isActive['tag']),
+//    array('label' => 'Ditandai', 'target' => 'tag', 'icon' => 'fa-tag', 'warna' => '#FC6800', 'isActive' => @$isActive['tag']),
 );
 $theUser = array(
     "namaUser" => "Ciprut",
